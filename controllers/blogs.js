f142b2a63b5d3cef5cc5dd5853c279626774de0e
Fabio Kim <fabio.kim@d2l.com>
2021-06-29 00:47:17 -0400

    added token auth

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,11 +2,21 @@ const blogsRouter = require('express').Router();
 const { response } = require('express');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7);
+    }
+    return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {
         username: 1,
-        name: 1
+        name: 1,
+        author
     });
 
     response.json(blogs);
@@ -23,11 +33,15 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body;
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    console.log(decodedToken);
 
-    const user = await User.findOne({}, {}, { sort: { created_at: -1 } }, function (err, post) {
-        console.log(post);
-    });
-    console.log(user);
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
         url: body.url,
@@ -37,6 +51,9 @@ blogsRouter.post('/', async (request, response) => {
     });
 
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
 });
 
